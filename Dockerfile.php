3c6db8541706d2a2ce9d64022cc2fa06d0bea326
Simon KP <skp@airtame.com>
2018-08-28 15:37:37 +1000

    Add php-ldap module

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,4 +3,12 @@ FROM ${CLI_IMAGE} as cli
 
 FROM amazeeio/php:7.2-fpm
 
+# Install needed php extensions: ldap
+RUN \
+    apk update && \
+    apk add openldap-dev && \
+    rm -rf /var/cache/apk/* && \
+    docker-php-ext-configure ldap --with-libdir=lib/ && \
+    docker-php-ext-install ldap
+
 COPY --from=cli /app /app
